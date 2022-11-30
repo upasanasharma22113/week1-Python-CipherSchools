97ac17d30ea4cc276e60111fb084f91d6e93dc09
Upasana Sharma <114800357+upasanasharma22113@users.noreply.github.com>
2022-12-01 01:11:48 +0530

    Add files via upload
    and or

---
+++ exercise4.py
@@ -0,0 +1,8 @@
+# COCO
+user_name= input("enter your name : ")
+user_age= input("enter your age: ")
+user_age=int(user_age)
+if user_age >= 10 and (user_name[0]=='a' or user_name[0]== 'A'):
+    print("you can watch coco")
+else:
+    print("you can not watch")
