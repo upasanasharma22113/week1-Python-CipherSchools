c375cca8475471513e6c7d10ae49749fc094d657
Upasana Sharma <114800357+upasanasharma22113@users.noreply.github.com>
2022-11-30 23:26:21 +0530

    Add files via upload
    another exercise

---
+++ exercise2.2.py
@@ -0,0 +1,13 @@
+name= input("enter your name : ")
+reverse= name[-1::-1]
+print(reverse)
+
+#another way
+print(f"reverse of your name is {reverse}")
+
+#another method
+print(f"reverse of your name is {name[-1::-1]}")
+
+
+
+
